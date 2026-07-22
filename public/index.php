26185a5e2e8fc6a414f549150c2beec1b1e4d1f4
eduardocapcha10-code <eduardocapcha10@gmail.com>
2026-07-21 20:22:19 -0500

    Hacer index.php inteligente para rutas dinámicas de cPanel

--- public/index.php
+++ public/index.php
@@ -6,15 +6,25 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../novape/storage/framework/maintenance.php')) {
+    require $maintenance;
+} elseif (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+if (file_exists(__DIR__.'/../novape/vendor/autoload.php')) {
+    require __DIR__.'/../novape/vendor/autoload.php'; // cPanel
+} else {
+    require __DIR__.'/../vendor/autoload.php'; // Local
+}
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+if (file_exists(__DIR__.'/../novape/bootstrap/app.php')) {
+    $app = require_once __DIR__.'/../novape/bootstrap/app.php'; // cPanel
+} else {
+    $app = require_once __DIR__.'/../bootstrap/app.php'; // Local
+}
 
 $app->handleRequest(Request::capture());
